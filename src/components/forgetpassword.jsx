0cb3bc1c1f6b59c0faf1a681c4d01652d76c5975
automationblog <sawin24@gmail.com>
2024-07-06 19:32:41 +0530

    routers added

---
+++ src/components/forgetpassword.jsx
@@ -0,0 +1,10 @@
+function ForgetPassword() {
+  return (
+    <div>
+      <h2>Forget Password</h2>
+      <p>Reset your password here.</p>
+    </div>
+  );
+}
+
+export default ForgetPassword;
---
+++ src/components/register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+  return (
+    <div>
+      <h2>Register</h2>
+      <p>Create your account here.</p>
+    </div>
+  );
+}
+
+export default Register;
