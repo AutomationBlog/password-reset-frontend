be0158d7a333f3750dd3794642cde46486e9c168
automationblog <sawin24@gmail.com>
2024-10-10 13:40:54 +0530

    dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import EmailVerificationPage from "./pages/EmailVerificationPage";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
 import { useAuthStore } from "./store/authStore";
+import DashboardPage from "./pages/DashboardPage";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+  return children;
+};
 
 const RedirectAuthenticatedUser = ({ children }) => {
   const { isAuthenticated, user } = useAuthStore();
   if (isAuthenticated && user.isVerified) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/dashboard" replace />;
   }
   return children;
 };
 
 function App() {
-  const { isCheckingAuth, isAuthenticated, user, checkAuth } = useAuthStore();
+  const { checkAuth } = useAuthStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -25,6 +37,14 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
           path="/signup"
           element={
             <RedirectAuthenticatedUser>
@@ -32,7 +52,14 @@ function App() {
             </RedirectAuthenticatedUser>
           }
         />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <LoginPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
         <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
       <Toaster />
